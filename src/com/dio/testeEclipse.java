e3469fb45abd41b80683188a51d2a636195bfa74
Rodrigo <rodrigo.marquesg@gmail.com>
2023-02-24 20:23:19 -0300

    first commit

---
+++ src/com/dio/testeEclipse.java
@@ -0,0 +1,20 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class testeEclipse {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+
+	}
+
+}
+
+class Livros {
+	
+}
